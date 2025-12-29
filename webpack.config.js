2998d0a9eb217d3fe86874f1352a8077331d136b
lray138 <57813289+lray138@users.noreply.github.com>
2025-12-28 21:19:36 -0500

    Webpack + Bootstrap - Section 2

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,54 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
+    entry: './src/js/index.js',
     plugins: [
         new HtmlWebpackPlugin(),
-    ]
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.(scss)$/,
+                use: [
+                    {
+                        // inject CSS to page
+                        loader: 'style-loader'
+                    }, {
+                        // translates CSS into CommonJS modules
+                        loader: 'css-loader'
+                    }, {
+                        // Run postcss actions
+                        loader: 'postcss-loader',
+                        options: {
+                            // `postcssOptions` is needed for postcss 8.x;
+                            // if you use postcss 7.x skip the key
+                            postcssOptions: {
+                                // postcss plugins, can be exported to postcss.config.js
+                                plugins: function () {
+                                    return [
+                                        require('autoprefixer')
+                                    ];
+                                }
+                            }
+                        }
+                    }, {
+                        // compiles Sass to CSS
+                        loader: 'sass-loader',
+                        options: {
+    sassOptions: {
+    // Optional: Silence Sass deprecation warnings. See note below.
+    silenceDeprecations: [
+        //'mixed-decls',
+        'color-functions',
+        'global-builtin',
+        'import'
+    ],
+    quietDeps: true,
+    }
+}
+                    }
+                ]
+            }
+        ]
+    }
 };
