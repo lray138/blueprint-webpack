0b9abaaa5eb4484d7932f1a450895c421de3884a
lray138 <57813289+lray138@users.noreply.github.com>
2025-12-29 12:55:47 -0500

    Bootpack Section 4 complete

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    entry: './src/js/index.js',
+    entry: './src/js/theme.js',
     plugins: [
         new HtmlWebpackPlugin(),
     ],
