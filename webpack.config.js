5f47fb3bd0a0351ec664d8ae6a9c3f1e6b554636
lray138 <57813289+lray138@users.noreply.github.com>
2026-02-09 14:00:42 -0500

    doc buildout

--- webpack.config.js
+++ webpack.config.js
@@ -55,7 +55,7 @@ module.exports = {
           filename: `./${pageName}.html`,
           templateParameters: {
             readMarkdown
-          }``
+          }
         });
       });
     })() : []),
