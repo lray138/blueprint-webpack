a6b1d8d656207b749948f327ea7749999487927d
lray138 <57813289+lray138@users.noreply.github.com>
2026-06-15 17:07:17 -0400

    removing webpack dev from being fired up automatically

---
+++ src/carbon-cms-project/js/theme.js
@@ -0,0 +1,11 @@
+import '@site/scss/_theme.scss';
+
+import '@blueprint/js/theme-base';
+
+// Bootstrap JS — add plugins as site patterns require them
+import 'bootstrap/js/dist/collapse';
+import 'bootstrap/js/dist/dropdown';
+import 'bootstrap/js/dist/offcanvas';
+
+import '@blueprint/js/theme-gallery';
+import '@blueprint/js/theme-flickity';
